ab6c1aa99d433d2caaba414b72b530860a6b9644
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 19:47:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+  return a+b
+}
+function subtract(b,a){
+  return b-a
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function inc(608){
+  return 608++
+}
+function dec(n){
+  return n--
+}
